51835b58d6e82f90feccf553ad882b9872b88b5b
Rakeshps901 <adithyaraj012003@gmail.com>
2023-08-03 14:39:25 +0530

    initial

--- lib/listview.dart
+++ lib/listview.dart
@@ -1,28 +1,54 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-void main()
-{
-  runApp(MaterialApp(home: ListView1(),));
+void main() {
+  runApp(MaterialApp(
+    home: ListView1(),
+  ));
 }
+
 class ListView1 extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
       appBar: AppBar(
         title: Text("ListView"),
       ),
       body: ListView(
         children: [
-          ListTile(
-            title: Text("HamBurger"),
-            subtitle: Text("\$190"),
-            trailing: Icon(Icons.shopping_cart),
-            leading: Image.asset("assets/images/download.jpg"),
+          Text("Select your Food",style: GoogleFonts.pacifico(fontWeight: FontWeight.bold,fontSize: 30,color: Colors.black),),
+          Card(
+            child: ListTile(
+              title: Text("HamBurger"),
+              subtitle: Text("\$190"),
+              trailing: Icon(Icons.shopping_cart),
+              leading: Image.asset("assets/images/ham.png") ,
+            ),
           ),
-          Text("Item 2"),
-          Text("Item 3"),
-          Text("Item 4"),
-          Text("Item 5"),
+          Card(
+            child: ListTile(
+              title: Text("Pizza"),
+              subtitle: Text("\$350"),
+              trailing: Icon(Icons.shopping_cart,color: Colors.teal,),
+              leading: Image.asset("assets/images/pizza.png") ,
+            ),
+          ),
+          Card(
+            child: ListTile(
+              title: Text("Salad"),
+              subtitle: Text("\$400"),
+              trailing: Icon(Icons.shopping_cart),
+              leading: Image.asset("assets/images/salad.png") ,
+            ),
+          ),
+          Card(
+            child: ListTile(
+              title: Text("Cake"),
+              subtitle: Text("\$400"),
+              trailing: Icon(Icons.shopping_cart),
+              leading: Image.asset("assets/images/cake.png") ,
+            ),
+          ),
         ],
       ),
     );
